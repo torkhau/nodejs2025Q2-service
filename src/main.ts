7ee6d5d01d7b0364c829842ebdec62d62a62d954
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-06-02 18:18:17 +0200

    feat: add Swagger support and YAML configuration for API documentation
    - Added @nestjs/swagger dependency to package.json for Swagger integration.
    - Included @types/yamljs for YAML file type definitions.
    - Updated main.ts to load API documentation from a YAML file and set up Swagger module.
    - Configured global validation pipe for request validation.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import 'dotenv/config';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
+import { join } from 'node:path';
+import * as YAML from 'yamljs';
+import { AppModule } from './app.module';
 
 const PORT = 4000;
+console.log(join(__dirname, '..', 'doc', 'api.yaml'));
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const openAPIObject = YAML.load(
+    join(__dirname, '..', 'doc', 'api.yaml'),
+  ) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, openAPIObject);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? PORT);
 }
